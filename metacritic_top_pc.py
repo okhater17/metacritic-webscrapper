a5c136babccf0fbd4f86d62ac725d6ba597557e1
okhater17 <okhater@fandm.edu>
2022-06-15 13:35:13 -0400

    added interface

--- metacritic_top_pc.py
+++ metacritic_top_pc.py
@@ -1,4 +1,3 @@
-import requests
 from requests import get
 from bs4 import BeautifulSoup
 def metacritic_web_scrapper(time, platform):
@@ -11,13 +10,18 @@ def metacritic_web_scrapper(time, platform):
     ans = "" #change var name
     for i in range (0, len(games), 2):
         ans += str((i + 2)//2) + ". " + games[i].h3.text + ", critics rating: " + games[i].div.findChild('div').text + "\n\n"
-    return ans
+    return ans[0: len(ans) - 2]
 
 def write_to_file(games):
     return
 
 def main():
-    #time = input("")
-    print(metacritic_web_scrapper("all", "wii-u"))
+    platforms = ["ps2", "ps3", "ps4", "pc", "xbox360", "xboxone", "xbox-series-x", "n64", "switch", "wii", "wii-u",
+                 "3ds"]
+    print("1. PS2\n2. PS3\n3. PS4\n4. PC\n5. Xbox 360\n6. Xbox One\n7. Xbox Series X\n8. N64\n9. Switch\n10. Wii\n"
+          "11. Wii-U\n12. 3DS")
+    platform = int(input("Please pick a platform by typing in the corresponding number"))
+    print(platforms[platform - 1])
+    print(metacritic_web_scrapper("all", platforms[platform - 1]))
 
 main()
